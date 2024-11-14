b9e1fea80385b367d60e8bba80fed3edeb608863
Shamil Siddique <shamilsdq@live.com>
2024-11-15 03:15:23 +0530

    Setup middleware to verify auth and add user detail to request

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,25 @@
+const JWT = require("jsonwebtoken");
+
+const authenticateToken = (req, res, next) => {
+  try {
+    const authHeader = req.headers.authorization;
+    if (!authHeader?.startsWith("Bearer ")) {
+      return res.status(401).json({ error: "No token provided" });
+    }
+
+    const token = authHeader.split(" ")[1];
+    const decoded = JWT.verify(token, process.env.JWT_SECRET);
+    req.userId = decoded.id;
+    next();
+  } catch (error) {
+    if (error.name === "TokenExpiredError") {
+      return res.status(401).json({ message: "Token expired" });
+    }
+    if (error.name === "JsonWebTokenError") {
+      return res.status(401).json({ message: "Invalid token" });
+    }
+    return res.status(500).json({ message: "Server error" });
+  }
+};
+
+module.exports = { authenticateToken };
