363dc69940bfc889394c4be70009da6e24883409
Shamil Siddique <shamilsdq@live.com>
2024-12-11 18:58:42 +0530

    Added employee routes in app router

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,12 @@ const express = require("express");
 const authRoutes = require("./auth.routes");
 const companyRoutes = require("./company.routes");
 const clientRoutes = require("./client.routes");
+const employeeRoutes = require("./employee.routes");
 
 const router = express.Router();
 router.use("/auth", authRoutes);
 router.use("/companies", companyRoutes);
 router.use("/clients", clientRoutes);
+router.use("/employees", employeeRoutes);
 
 module.exports = router;
